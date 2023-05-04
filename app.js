a89b9780581218d3b67a90d5e1aefadf85c24011
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2023-05-04 16:39:05 +0700

    test: mongodb uri

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+console.log(process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
